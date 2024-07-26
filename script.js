457d875b422d5a2ce2bee18b95dcf884f1547ca3
Sayhan Rahman <sayhanrahman@gokkusagi.k12.tr>
2024-07-26 22:40:34 +0600

    More Updates wuth sound and patterns

--- script.js
+++ script.js
@@ -1,21 +1,45 @@
+const clickSound = new Audio('/sounds/click.mp3');
+
+function playClickSound() {
+    clickSound.currentTime = 0; // Rewind to the start
+    clickSound.play();
+}
+
+function handleClick(value) {
+    playClickSound();
+    switch(value) {
+        case 'C':
+            clearDisplay();
+            break;
+        case 'DEL':
+            deleteLastDigit();
+            break;
+        case '=':
+            calculate();
+            break;
+        default:
+            appendToDisplay(value);
+    }
+}
+
 function appendToDisplay(value) {
     document.getElementById('display').innerText += value;
-  }
+}
 
-  function clearDisplay() {
+function clearDisplay() {
     document.getElementById('display').innerText = '';
-  }
+}
 
-  function deleteLastDigit() {
+function deleteLastDigit() {
     const display = document.getElementById('display');
     display.innerText = display.innerText.slice(0, -1);
-  }
+}
 
-  function calculate() {
+function calculate() {
     const display = document.getElementById('display');
     try {
-      display.innerText = eval(display.innerText);
+        display.innerText = eval(display.innerText);
     } catch {
-      display.innerText = 'Error';
+        display.innerText = 'Error';
     }
-  }
+}
